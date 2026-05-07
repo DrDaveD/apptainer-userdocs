c3bbac3beb43e4ca838e0546622c98f9a1a17279
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2026-05-07 13:16:16 -0500

    update replacements for 1.5
    Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

--- replacements.py
+++ replacements.py
@@ -13,16 +13,16 @@ def variableReplace(app, docname, source):
 # dictionary structure.
 variable_replacements = {
     "{InstallationVersion}" : "1.5.0",
-    "{admindocs}" : "https://apptainer.org/docs/admin/main",
-    "{version}": "main",
-    "{adminversion}": "main",
+    "{admindocs}" : "https://apptainer.org/docs/admin/1.5",
+    "{version}": "1.5",
+    "{adminversion}": "1.5",
     "{Project}": "Apptainer",
     "{AProject}": "An Apptainer",
     "{aProject}": "an Apptainer",
     "{command}": "apptainer",
     "{ENVPREFIX}": "APPTAINER",
     "{orgrepo}": "apptainer/apptainer",
-    "{repobranch}": "main",
+    "{repobranch}": "release-1.5",
 }
 
 
